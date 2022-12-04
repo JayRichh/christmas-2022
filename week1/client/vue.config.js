d5a28cedd91dad61c88e4c86aff28d47da103dcf
Jayden <jsrich@hotmail.co.nz>
2022-12-04 22:56:12 +0000

    update url

--- week1/client/vue.config.js
+++ week1/client/vue.config.js
@@ -1,12 +1,11 @@
 // const { defineConfig } = require("@vue/cli-service");
 module.exports = {
-  runtimeCompiler: true,
   configureWebpack: {
     devServer: {
       proxy: {
         "/api": {
           target: 
-            "https://638d20c61c52d87459600b90--soft-cendol-429c0f.netlify.app",
+            "http://localhost:3000",
         },
       },
     },
