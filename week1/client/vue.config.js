5926f6a46cd5e87727689f72c400d4ef50fa0930
Jayden <jsrich@hotmail.co.nz>
2022-12-04 22:20:55 +0000

    update url

--- week1/client/vue.config.js
+++ week1/client/vue.config.js
@@ -1,11 +1,11 @@
 // const { defineConfig } = require("@vue/cli-service");
 module.exports = {
-  runtimeCompiler: true,
   configureWebpack: {
     devServer: {
       proxy: {
         "/api": {
-          target: "elf-gp9m-1j4cyf3jf-jayrichh.vercel.app",
+          target: 
+            "http://localhost:3000",
         },
       },
     },
