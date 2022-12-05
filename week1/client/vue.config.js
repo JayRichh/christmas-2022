b90607a23e1bdddf13ad0df9bd4bf62106eea526
Jayden <jsrich@hotmail.co.nz>
2022-12-05 01:23:06 +0000

    fix commas

--- week1/client/vue.config.js
+++ week1/client/vue.config.js
@@ -4,11 +4,11 @@ module.exports = {
   runtimeCompiler: true,
   devServer: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
+      "/api": {
+        target: "http://localhost:3000",
         ws: true,
-        changeOrigin: true
-      }
-    }
+        changeOrigin: true,
+      },
+    },
   },
 };
